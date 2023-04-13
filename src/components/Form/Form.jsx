f196e51ac79e78069f695b32f2020d299a121843
Prysiazhnyi Daniil <prysiazhnyi.d@gmail.com>
2023-04-13 16:13:53 +0300

    qwe

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -42,19 +42,19 @@ export const Form = () => {
         setCountry(e.target.value)
     }
 
-    const onChangestreet = (e) => {
-        setCountry(e.target.value)
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
     }
 
     const onChangeSubject = (e) => {
-        setCountry(e.target.value)
+        setSubject(e.target.value)
     }
 
   return (
     <div>
         <h3>Введите ваши данные</h3>
         <input onChange={onChangeCountry} className={styles.input} type="text" placeholder={"Страна"} value={country} />
-        <input onChange={onChangestreet} className={styles.input} type="text" placeholder={"Улица"} value={street} />
+        <input onChange={onChangeStreet} className={styles.input} type="text" placeholder={"Улица"} value={street} />
         <select value={subject} onChange={onChangeSubject} className={styles.select}>
             <option value="physical">Юр лицо</option>
             <option value="legal">Физ лицо</option>
