1608cf82755b44d639b1bb7d67cd328941dcc8ef
Prysiazhnyi Daniil <prysiazhnyi.d@gmail.com>
2023-04-13 14:07:51 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 import './App.css';
-import {Button} from './components/Button/Button'
+import {Button} from './components/Button/Button';
+import { useTelegram } from './hooks/useTelegram';
+import {Header} from './components/Header/Header';
 
 function App() {
-
-  const tg = window.Telegram.WebApp;
+  const {onToggleButton, tg} = useTelegram()
 
   useEffect(() => {
     tg.ready();
@@ -13,7 +14,8 @@ function App() {
   return (
     <div className="App">
       test
-      <Button onClick={onClose}>Закрыть</Button>
+      <Header />
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
---
+++ src/hooks/useTelegram.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const tg = window.Telegram.WebApp;
+
+export const useTelegram = () => {
+
+    const onClose = () => {
+        tg.close();
+    }
+
+    const onToggleButton = () => {
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
+
+  return {
+    onToggleButton,
+    onClose,
+    tg,
+    user: tg.initDataUnsafe?.userm,
+  }
+}
